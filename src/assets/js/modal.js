7fdc2297707677d44bb4a59f6e9812e315b9f027
qhouguet <quentin.houguet@gmail.com>
2024-03-08 13:34:24 +0100

    Update structure + ajout close modal + label

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,4 +1,4 @@
-function editNav() {
+const editNav = () => {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -8,16 +8,20 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
 }
-
-
+// hide modal form
+function closeModal() {
+  modalBg.style.display = "none";
+}
